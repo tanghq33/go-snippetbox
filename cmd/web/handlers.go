d46ee22a2d363727de9717909b206f540469dc0f
haoquantang <hqtang33@gmail.com>
2023-09-03 19:27:40 +0800

    Chapter 5.2

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -22,36 +22,40 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		app.serverError(w, err)
 		return
 	}
-	for _, snippet := range snippets {
-		fmt.Fprintf(w, "%+v\n", snippet)
+
+	// Initialize a slice containing the paths to the two files. It's important
+	// to note that the file containing our base template must be the *first*
+	// file in the slice.
+	files := []string{
+		"./ui/html/base.tmpl.html",
+		"./ui/html/pages/home.tmpl.html",
+		"./ui/html/partials/nav.tmpl.html",
 	}
 
-	// // Initialize a slice containing the paths to the two files. It's important
-	// // to note that the file containing our base template must be the *first*
-	// // file in the slice.
-	// files := []string{
-	// 	"./ui/html/base.tmpl.html",
-	// 	"./ui/html/pages/home.tmpl.html",
-	// 	"./ui/html/partials/nav.tmpl.html",
-	// }
+	// Use the template.ParseFiles() function to read the template file into a
+	// template set. If there's an error, we log the detailed error message and use
+	// the http.Error() function to send a generic 500 Internal Server Error
+	// response to the user.
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
 
-	// // Use the template.ParseFiles() function to read the template file into a
-	// // template set. If there's an error, we log the detailed error message and use
-	// // the http.Error() function to send a generic 500 Internal Server Error
-	// // response to the user.
-	// ts, err := template.ParseFiles(files...)
-	// if err != nil {
-	// 	app.serverError(w, err)
-	// 	return
-	// }
-	// // We then use the Execute() method on the template set to write the
-	// // template content as the response body. The last parameter to Execute()
-	// // represents any dynamic data that we want to pass in, which for now we'll
-	// // leave as nil.
-	// err = ts.ExecuteTemplate(w, "base", nil)
-	// if err != nil {
-	// 	app.serverError(w, err)
-	// }
+	// Create an instance of a templateData struct holding the slice of
+	// snippets.
+	data := &templateData{
+		Snippets: snippets,
+	}
+
+	// We then use the Execute() method on the template set to write the
+	// template content as the response body. The last parameter to Execute()
+	// represents any dynamic data that we want to pass in, which for now we'll
+	// leave as nil.
+	err = ts.ExecuteTemplate(w, "base", data)
+	if err != nil {
+		app.serverError(w, err)
+	}
 }
 
 // Change the signature of the snippetView handler so it is defined as a method
