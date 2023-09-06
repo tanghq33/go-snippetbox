ddd7ec2d376af7350a48e51bb7024e24c8abe08c
haoquantang <hqtang33@gmail.com>
2023-09-06 21:17:40 +0800

    Completed chapter 8.1

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -71,7 +71,8 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 // Add a new snippetCreate handler, which for now returns a placeholder
 // response. We'll update this shortly to show a HTML form.
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display the form for creating a new snippet..."))
+	data := app.newTemplateData(r)
+	app.render(w, http.StatusOK, "create.tmpl.html", data)
 }
 
 // Change the signature of the snippetCreate handler so it is defined as a method
